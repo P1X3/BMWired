d8cea6d1516c978de819a69eddd6151ef76f1129
Vadim <vosovskiy@gmail.com>
2014-08-29 14:43:19 -0700

    Send dummy message to bus when activity is started

--- dummy/src/main/java/com/osovskiy/bmwinterface/dummy/MyActivity.java
+++ dummy/src/main/java/com/osovskiy/bmwinterface/dummy/MyActivity.java
@@ -1,12 +1,10 @@
 package com.osovskiy.bmwinterface.dummy;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.osovskiy.bmwinterface.lib.BusMessage;
+import com.osovskiy.bmwinterface.lib.Utils;
 
 
 public class MyActivity extends Activity
@@ -15,6 +13,9 @@ public class MyActivity extends Activity
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
+
+    BusMessage msg = new BusMessage(BusMessage.BusDevice.Broadcast, BusMessage.BusDevice.CD, new byte[] { 0x00, 0x0C, (byte)0xFB, 0x01 });
+    sendBroadcast(msg.getIntent(Utils.ACTION_SEND_BUS_MESSAGE), Utils.PERMISSION_SEND_MESSAGE);
     finish();
   }
 }
