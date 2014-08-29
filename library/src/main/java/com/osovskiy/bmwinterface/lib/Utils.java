ee57875dc8631a09030e7fd10ba7c41d1192004b
Vadim <vosovskiy@gmail.com>
2014-08-29 14:39:41 -0700

    Added method for parsing hexadecimal string to byte array

--- library/src/main/java/com/osovskiy/bmwinterface/lib/Utils.java
+++ library/src/main/java/com/osovskiy/bmwinterface/lib/Utils.java
@@ -10,4 +10,15 @@ public class Utils
 
   public static final String PERMISSION_SEND_MESSAGE = "com.osovskiy.bmwinterface.permission.SEND_MSG_PERMISSION";
   public static final String PERMISSION_RECEIVE_MESSAGE = "com.osovskiy.bmwinterface.permission.SEND_RECEIVE_PERMISSION";
+
+  public static byte[] hexStringToByteArray(String s)
+  {
+    int len = s.length();
+    byte[] data = new byte[len / 2];
+    for (int i = 0; i < len; i += 2) {
+      data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+          + Character.digit(s.charAt(i+1), 16));
+    }
+    return data;
+  }
 }
