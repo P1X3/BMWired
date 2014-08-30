38342aa69da9ba2a02ae6d46f07e672ee9ab3994
Vadim <vosovskiy@gmail.com>
2014-08-29 17:04:43 -0700

    Refactor permission name

--- library/src/main/java/com/osovskiy/bmwinterface/lib/Utils.java
+++ library/src/main/java/com/osovskiy/bmwinterface/lib/Utils.java
@@ -8,8 +8,8 @@ public class Utils
   public static final String ACTION_NEW_BUS_MESSAGE = "com.osovskiy.bmwinterface.ACTION_NEW_BUS_MESSAGE";
   public static final String ACTION_SEND_BUS_MESSAGE = "com.osovskiy.bmwinterface.ACTION_SEND_BUS_MESSAGE";
 
-  public static final String PERMISSION_SEND_MESSAGE = "com.osovskiy.bmwinterface.permission.SEND_MSG_PERMISSION";
-  public static final String PERMISSION_RECEIVE_MESSAGE = "com.osovskiy.bmwinterface.permission.RECEIVE_MSG_PERMISSION";
+  public static final String PERMISSION_SEND_MESSAGE = "com.osovskiy.bmwinterface.permission.SEND_MSG";
+  public static final String PERMISSION_RECEIVE_MESSAGE = "com.osovskiy.bmwinterface.permission.RECEIVE_MSG";
 
   public static byte[] hexStringToByteArray(String s)
   {
