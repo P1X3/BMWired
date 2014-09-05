75f52387a4c08f8087c2e43781b3c86eb6735a9f
Vadim <vosovskiy@gmail.com>
2014-09-04 20:58:15 -0700

    Refactored preferences fragment to disable and enable certain preferences dynamically.

--- core/src/main/java/com/osovskiy/bmwinterface/fragment/preferences/PreferencesFragment.java
+++ core/src/main/java/com/osovskiy/bmwinterface/fragment/preferences/PreferencesFragment.java
@@ -1,20 +1,60 @@
 package com.osovskiy.bmwinterface.fragment.preferences;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 
 import com.osovskiy.bmwinterface.R;
 
-/**
- * Created by Administrator on 8/22/2014.
- */
 public class PreferencesFragment extends PreferenceFragment
 {
+  SharedPreferences.OnSharedPreferenceChangeListener onSharedPreferenceChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener()
+  {
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+    {
+      setupPreferences(sharedPreferences);
+    }
+  };
+
   @Override
   public void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
-
     addPreferencesFromResource(R.xml.preferences);
+    setupPreferences(getPreferenceManager().getSharedPreferences());
+  }
+
+  @Override
+  public void onResume()
+  {
+    super.onResume();
+    getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener);
+  }
+
+  @Override
+  public void onPause()
+  {
+    getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener);
+    super.onPause();
+  }
+
+  private void setupPreferences(SharedPreferences sharedPreferences)
+  {
+    if (sharedPreferences.getBoolean("bluetooth_interface", false))
+    {
+      PreferenceScreen preferenceScreen = getPreferenceScreen();
+      preferenceScreen.findPreference("bluetooth_mac").setEnabled(true);
+      preferenceScreen.findPreference("bluetooth_attempts").setEnabled(true);
+      preferenceScreen.findPreference("bluetooth_delay").setEnabled(true);
+    }
+    else
+    {
+      PreferenceScreen preferenceScreen = getPreferenceScreen();
+      preferenceScreen.findPreference("bluetooth_mac").setEnabled(false);
+      preferenceScreen.findPreference("bluetooth_attempts").setEnabled(false);
+      preferenceScreen.findPreference("bluetooth_delay").setEnabled(false);
+    }
   }
 }
