97ea4b37d214b8d431f8a78dc8606870a7b9418d
Vadim <vosovskiy@gmail.com>
2014-09-15 00:27:25 -0700

    Service connection removed from activity

--- core/src/main/java/com/osovskiy/bmwired/MainActivity.java
+++ core/src/main/java/com/osovskiy/bmwired/MainActivity.java
@@ -5,13 +5,9 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.ComponentName;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.Messenger;
 import android.util.Log;
 
 import com.osovskiy.bmwired.fragment.debugging.DebuggingFragment;
@@ -29,54 +25,12 @@ public class MainActivity extends Activity implements ActionBar.TabListener
   private static final String TAG = MainActivity.class.getSimpleName();
   private Map<Integer, Class<? extends Fragment>> fragments = new LinkedHashMap<>();
 
-  private boolean serviceBound = false;
-  private Messenger serviceMessenger = null;
-
-  private ServiceConnection serviceConnection = new ServiceConnection()
-  {
-    @Override
-    public void onServiceConnected(ComponentName name, IBinder service)
-    {
-      Log.d(TAG, "Connected to the service");
-      serviceMessenger = new Messenger(service);
-      serviceBound = true;
-    }
-
-    @Override
-    public void onServiceDisconnected(ComponentName name)
-    {
-      Log.d(TAG, "Disconnected from the service");
-      serviceMessenger = null;
-      serviceBound = false;
-    }
-  };
-
   public MainActivity()
   {
     fragments.put(R.string.tab_plugins, PluginsFragment.class);
     fragments.put(R.string.tab_preferences, PreferencesFragment.class);
     fragments.put(R.string.tab_setup, SetupFragment.class);
     fragments.put(R.string.tab_debugging, DebuggingFragment.class);
-  }
-
-  @Override
-  protected void onStart()
-  {
-    super.onStart();
-
-    bindService(new Intent(this, BMWiService.class), serviceConnection, 0);
-  }
-
-  @Override
-  protected void onStop()
-  {
-    super.onStop();
-
-    if ( serviceBound )
-    {
-      unbindService(serviceConnection);
-      serviceBound = false;
-    }
   }
 
   @Override
@@ -100,7 +54,7 @@ public class MainActivity extends Activity implements ActionBar.TabListener
     setContentView(R.layout.activity_main);
 
     ActionBar actionBar = getActionBar();
-    if (actionBar != null)
+    if ( actionBar != null )
     {
       actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
@@ -122,13 +76,9 @@ public class MainActivity extends Activity implements ActionBar.TabListener
     Fragment fragment = null;
     try
     {
-      Class<? extends Fragment> fragmentClass = fragments.get((Integer)tab.getTag());
+      Class<? extends Fragment> fragmentClass = fragments.get((Integer) tab.getTag());
       Constructor<? extends Fragment> fragmentConstructor = fragmentClass.getConstructor();
       fragment = fragmentConstructor.newInstance();
-
-      Bundle bundle = new Bundle();
-      bundle.putParcelable(Messenger.class.getSimpleName(), serviceMessenger);
-      fragment.setArguments(bundle);
     }
     catch ( NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e )
     {
