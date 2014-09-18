fa54dffb957b94eee3c0c933be9950ab9ff30aef
Vadim <vosovskiy@gmail.com>
2014-09-18 01:03:16 -0700

    Relying on support library. New sliding tab and fragment view pager.

--- core/src/main/java/com/osovskiy/bmwired/MainActivity.java
+++ core/src/main/java/com/osovskiy/bmwired/MainActivity.java
@@ -1,37 +1,31 @@
 package com.osovskiy.bmwired;
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 
-import com.osovskiy.bmwired.fragment.debugging.DebuggingFragment;
+import com.osovskiy.bmwired.fragment.FragmentAdapter;
 import com.osovskiy.bmwired.fragment.plugins.PluginsFragment;
 import com.osovskiy.bmwired.fragment.preferences.PreferencesFragment;
 import com.osovskiy.bmwired.fragment.setup.SetupFragment;
+import com.osovskiy.bmwired.view.SlidingTabLayout;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.LinkedHashMap;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends Activity implements ActionBar.TabListener
+public class MainActivity extends FragmentActivity
 {
   private static final String TAG = MainActivity.class.getSimpleName();
-  private Map<Integer, Class<? extends Fragment>> fragments = new LinkedHashMap<>();
+  List<Map.Entry<String, Fragment>> fragments = new ArrayList<>();
 
-  public MainActivity()
-  {
-    fragments.put(R.string.tab_plugins, PluginsFragment.class);
-    fragments.put(R.string.tab_preferences, PreferencesFragment.class);
-    fragments.put(R.string.tab_setup, SetupFragment.class);
-    fragments.put(R.string.tab_debugging, DebuggingFragment.class);
-  }
+  FragmentAdapter adapter;
+  ViewPager viewPager;
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
@@ -50,61 +44,15 @@ public class MainActivity extends Activity implements ActionBar.TabListener
     }
 
     super.onCreate(savedInstanceState);
-
     setContentView(R.layout.activity_main);
 
-    ActionBar actionBar = getActionBar();
-    if ( actionBar != null )
-    {
-      actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+    fragments.add(new AbstractMap.SimpleEntry<String, Fragment>(getString(R.string.tab_plugins), new PluginsFragment()));
+    fragments.add(new AbstractMap.SimpleEntry<String, Fragment>(getString(R.string.tab_preferences), new PreferencesFragment()));
+    fragments.add(new AbstractMap.SimpleEntry<String, Fragment>(getString(R.string.tab_setup), new SetupFragment()));
 
-      for ( Map.Entry<Integer, Class<? extends Fragment>> e : fragments.entrySet() )
-      {
-        ActionBar.Tab tab = actionBar.newTab();
-        tab.setTag(e.getKey());
-        tab.setText(getString(e.getKey()));
-        tab.setTabListener(this);
-        actionBar.addTab(tab);
-      }
-    }
-  }
-
-  @Override
-  public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft)
-  {
-    Log.d(TAG, "Tab \"" + tab.getText() + "\" selected");
-
-    FragmentManager fragmentManager = getFragmentManager();
-    Fragment fragment = fragmentManager.findFragmentByTag(String.valueOf(tab.getTag()));
-
-    if (fragment == null)
-    {
-      try
-      {
-        Class<? extends Fragment> fragmentClass = fragments.get((Integer) tab.getTag());
-        Constructor<? extends Fragment> fragmentConstructor = fragmentClass.getConstructor();
-        fragment = fragmentConstructor.newInstance();
-      }
-      catch ( NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e )
-      {
-        e.printStackTrace();
-      }
-    }
-
-    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-    fragmentTransaction.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
-    fragmentTransaction.replace(R.id.fragment_container, fragment, String.valueOf(tab.getTag())).addToBackStack(null).commit();
-  }
-
-  @Override
-  public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft)
-  {
-
-  }
-
-  @Override
-  public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft)
-  {
-
+    adapter = new FragmentAdapter(getSupportFragmentManager(), fragments);
+    viewPager = (ViewPager) findViewById(R.id.viewpager);
+    viewPager.setAdapter(adapter);
+    ((SlidingTabLayout) findViewById(R.id.sliding_tabs)).setViewPager(viewPager);
   }
 }
